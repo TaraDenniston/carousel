804172f0aa39163f549eeef3102acf24e6a0be12
TaraDenniston <taradenniston@gmail.com>
2025-10-23 15:37:58 -0400

    clean up code

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,6 @@
-import { expect, afterEach } from 'vitest';
+import {afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-
-// Extends Vitest's expect with methods from react-testing-library
-// expect.extend(matchers); <--does not work
 
 // Runs a cleanup after each test case (e.g., clearing jsdom)
 afterEach(() => {
