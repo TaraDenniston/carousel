8420e1f2432d9f40d9e5e0e26aef4b9e6e1e7743
TaraDenniston <taradenniston@gmail.com>
2025-10-23 13:06:40 -0400

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Carousel</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/setupTests.js
@@ -0,0 +1,10 @@
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+
+// Extends Vitest's expect with methods from react-testing-library
+// expect.extend(matchers); <--does not work
+
+// Runs a cleanup after each test case (e.g., clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
