7092264fe25ef7144c8e11a4955e7347b475b2e5
TaraDenniston <taradenniston@gmail.com>
2025-10-24 12:59:43 -0400

    added test for left arrow

--- src/components/Carousel.test.jsx
+++ src/components/Carousel.test.jsx
@@ -26,5 +26,13 @@ describe('Carousel', () => {
     // expect the second image to show, but not the first
     expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
     expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+    // move backward in the carousel
+    const leftArrow = queryByTestId("left-arrow");
+    fireEvent.click(leftArrow);
+
+    // expect the first image to show, but not the second
+    expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+    expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
   });
 });
